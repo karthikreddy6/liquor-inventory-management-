3b9a30a74dfbe3e41461093b5877635516f0c570
karth <karthikreddy4516@gmail.com>
2026-02-06 18:03:26 +0530

    credintal changed

--- config.py
+++ config.py
@@ -6,14 +6,14 @@ os.makedirs(INVOICES_FOLDER, exist_ok=True)
 
 APP_START_TIME = time.time()
 
-ADMIN_USER = os.getenv("ADMIN_USER", "admin")
-ADMIN_PASS = os.getenv("ADMIN_PASS", "admin123")
+ADMIN_USER = os.getenv("ADMIN_USER", "arjun")
+ADMIN_PASS = os.getenv("ADMIN_PASS", "1008984")
 
 JWT_SECRET = os.getenv("JWT_SECRET", "change-this-secret")
 JWT_ALGORITHM = "HS256"
 JWT_EXPIRES_MINUTES = int(os.getenv("JWT_EXPIRES_MINUTES", "480"))
 
 OWNER_USER = os.getenv("OWNER_USER", "owner")
-OWNER_PASS = os.getenv("OWNER_PASS", "owner123")
+OWNER_PASS = os.getenv("OWNER_PASS", "owner6060")
 SUPERVISOR_USER = os.getenv("SUPERVISOR_USER", "supervisor")
-SUPERVISOR_PASS = os.getenv("SUPERVISOR_PASS", "super123")
+SUPERVISOR_PASS = os.getenv("SUPERVISOR_PASS", "super6060")
