5fe38507fced4143d9eacc79de3cfda617ecb550
karth <karthikreddy4516@gmail.com>
2026-02-06 18:22:38 +0530

    user

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ os.makedirs(INVOICES_FOLDER, exist_ok=True)
 APP_START_TIME = time.time()
 
 ADMIN_USER = os.getenv("ADMIN_USER", "arjun")
-ADMIN_PASS = os.getenv("ADMIN_PASS", "1008984")
+ADMIN_PASS = os.getenv("ADMIN_PASS", "10081984")
 
 JWT_SECRET = os.getenv("JWT_SECRET", "change-this-secret")
 JWT_ALGORITHM = "HS256"
